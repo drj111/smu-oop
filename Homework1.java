4239724d57de9f9845983b7de2c3586254de3b5f
drj111 <an012345@naver.com>
2026-09-14 20:22:12 +0900

    week02

---
+++ Homework1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int num;
+
+        num = sc.nextInt();
+        sum += num;
+        System.out.println("누적: " + sum);
+
+        num = sc.nextInt();
+        sum += num;
+        System.out.println("누적: " + sum);
+
+        num = sc.nextInt();
+        sum += num;
+        System.out.println("누적: " + sum);
+
+        num = sc.nextInt();
+        sum += num;
+        System.out.println("누적: " + sum);
+
+        num = sc.nextInt();
+        sum += num;
+        System.out.println("누적: " + sum);
+    }
+}
