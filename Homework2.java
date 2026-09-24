db48f476e4a8dc93570d69a99043d54ad626fb50
drj111 <an012345@naver.com>
2026-09-24 15:58:05 +0900

    modify Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -1,47 +1,47 @@
 import java.util.Scanner;
 
-class Homework2 {
-    void main() {
-        Student st1 = new Student();
-        Student st2 = new Student();
-        Student st3 = new Student();
+class Student {
+    int studentID;
+    String name;
+    String major;
+    String phone;
 
-        System.out.println("입력된 학생들의 정보는 다음과 같습니다.");
-        System.out.printf("1번째 학생: %d %s %s %s\n", st1.getStudentNumber(), st1.getName(), st1.getMajor(), st1.getPhone());
-        System.out.printf("2번째 학생: %d %s %s %s\n", st2.getStudentNumber(), st2.getName(), st2.getMajor(), st2.getPhone());
-        System.out.printf("3번째 학생: %d %s %s %s\n", st3.getStudentNumber(), st3.getName(), st3.getMajor(), st3.getPhone());
+    int getStudentID() {return studentID;}
+    String getName() {return name;}
+    String getMajor() {return major;}
+    String getPhone() {return phone;}
+
+    void setStudentID(int studentID) {this.studentID = studentID;}
+    void setName(String name) {this.name = name;}
+    void setMajor(String major) {this.major = major;}
+    void setPhone(int phone) {
+        String strPhone = Integer.toString(phone);
+        this.phone = "0" + strPhone.substring(0, 2) + "-" + strPhone.substring(2, 6) + "-" + strPhone.substring(6, 10);
     }
 }
 
 
-class Student {
-    int studentNumber;
-    String name;
-    String major;
-    long phone;
-
+class Homework2 {
     Scanner sc = new Scanner(System.in);
 
-    void setStudentNumber() {studentNumber = sc.nextInt();}
-    void setName() {name = sc.next();}
-    void setMajor() {major = sc.next();}
-    void setPhone() {phone = sc.nextLong();}
+    Student st1 = new Student();
+    Student st2 = new Student();
+    Student st3 = new Student();
+    Student[] sArr = {st1, st2, st3};
 
-    int getStudentNumber() {return studentNumber;}
-    String getName() {return name;}
-    String getMajor() {return major;}
-    String getPhone() {
-        String strPhone;
-        strPhone = "0"+Long.toString(phone);
-        strPhone = strPhone.substring(0,3)+"-"+strPhone.substring(3,7)+"-"+strPhone.substring(7,11);
-        return strPhone;
-    }
+    void main() {
+        for (int i=0; i<=2; i++) {
+            System.out.print("학생의 학번, 이름, 전공, 전화번호를 입력하세요: ");
+            sArr[i].setStudentID(sc.nextInt());
+            sArr[i].setName(sc.next());
+            sArr[i].setMajor(sc.next());
+            sArr[i].setPhone(sc.nextInt());
+        }
 
-    Student() {
-        System.out.print("학생의 학번, 이름, 전공, 전화번호를 입력하세요: ");
-        setStudentNumber();
-        setName();
-        setMajor();
-        setPhone();
+        System.out.println("입력된 학생들의 정보는 다음과 같습니다.");
+
+        for (int i=0; i<=2; i++) {
+            System.out.printf("%d번째 학생: %d %s %s %s\n", i+1, sArr[i].getStudentID(), sArr[i].getName(), sArr[i].getMajor(), sArr[i].getPhone());
+        }
     }
 }
